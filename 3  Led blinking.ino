f4f78ef1edb7ebbcbbcff83321843ddb2db21277
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-05 00:33:14 +0600

    Update 3  Led blinking.ino

--- 3  Led blinking.ino
+++ 3  Led blinking.ino
@@ -16,11 +16,11 @@ void loop(){
   digitalWrite(led2,HIGH);
   delay (1000);
   digitalWrite(led2,LOW);
-   delay (1000);
+  delay (1000);
   
   digitalWrite(led3,HIGH);
   delay (1000);
   digitalWrite(led3,LOW);
-   delay (1000);
+  delay (1000);
   
 }
