9df20f1caaf19aec52628699d4ba9a1c4a335f09
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-15 03:04:49 +0600

    Update 1.Some project name.ino

--- 1.Some project name.ino
+++ 1.Some project name.ino
@@ -3,3 +3,4 @@
 3.automatic car parking toll gate system
 4.Arduino based automatic water tap usingultra sonic sensor and micro servo
 5.LDR Interfacing with Arduino
+6.IRRIGATION SYSTEM using the Arduino(Need to LCD display and I2C Module)
