8356296bac094f12e50113ff2860912f435cbb39
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-15 03:13:06 +0600

    Update 1.Some project name.ino

--- 1.Some project name.ino
+++ 1.Some project name.ino
@@ -4,3 +4,6 @@
 4.Arduino based automatic water tap usingultra sonic sensor and micro servo
 5.LDR Interfacing with Arduino
 6.IRRIGATION SYSTEM using the Arduino(Need to LCD display and I2C Module)
+7. tempreture metre with Arduino(Need to LCD display and I2C Module)
+8.Keypad Locking System(Need to LCD display)
+9.Temperature & Humidity Device(Need to LCD display)
