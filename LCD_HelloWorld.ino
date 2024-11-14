f78ab72a51ef6eed95a2d96916c3cf234bb3ef08
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-15 01:33:13 +0600

    Update LCD_HelloWorld.ino

--- LCD_HelloWorld.ino
+++ LCD_HelloWorld.ino
@@ -1,1 +1,15 @@
+#include<LiquidCrystal.h>
 
+//liquidCrystal lcd(rs,en,db4,db5,db6,db7)
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup(){
+  lcd.begin(16,2);
+  lcd.print("Hellow World");
+  lcd.setCursor(0,1);
+  lcd.print("Bangladesh");
+  
+}
+void loop(){
+}
