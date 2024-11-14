0f07bafc0afd3624d02530153b15958fc0f97120
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-14 23:01:27 +0600

    Update Sequential LED Blinker.ino

--- Sequential LED Blinker.ino
+++ Sequential LED Blinker.ino
@@ -1,1 +1,17 @@
+int i;
+
+void setup() {
+  for (i = 3; i <= 8; i++) {
+    pinMode(i, OUTPUT); 
+  }
+}
+
+void loop() {
+  for (i = 3; i <= 8; i++) {
+    digitalWrite(i, HIGH); 
+    delay(1000);          
+    digitalWrite(i, LOW);   
+    delay(1000);           
+  }
+}
 
