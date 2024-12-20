534f0e768996c7edc72fb801e00807d6e3d7ff15
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-20 12:09:16 +0600

    Update Stepper Motor.ino

--- Stepper Motor.ino
+++ Stepper Motor.ino
@@ -1,1 +1,16 @@
-ino
+#include <Stepper.h>
+#define STEPS 2048 
+
+Stepper stepper(STEPS, 8, 10, 9, 11);  //(STEPS,1st,3rd,2nd,4rd)
+
+void setup() {
+  stepper.setSpeed(6); 
+}
+
+void loop() {
+  stepper.step(1024);  // Rotate 180 degrees forward
+  delay(1000);         
+
+  stepper.step(-1024);
+  delay(1000);       
+}
