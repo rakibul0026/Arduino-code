05c43f2b9007d73752b1bd51e058b0815df7bfa2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-23 11:35:13 +0600

    Create global and local in arduino.ino

---
+++ global and local in arduino.ino
@@ -0,0 +1,10 @@
+int led1=12;  // global variable
+void setup() {
+  // put your setup code here, to run once:
+   int led2=13;  // local variable
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
