4cb5a36df03c63421db67cce535709f5612011a5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-25 01:22:54 +0600

    Update The Return Keyword.ino

--- The Return Keyword.ino
+++ The Return Keyword.ino
@@ -1,1 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2,checksensor());
+
+}
+int checksensor(){
+  int val=analogRead(A0);
+  Serial.println(val);
+  if(val>400){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
 
