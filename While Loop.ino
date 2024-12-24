8e17b58f5ee88c91eb532f6686de82a8a9ec30ed
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-25 00:12:35 +0600

    Create While Loop.ino

---
+++ While Loop.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4,OUTPUT);
+  pinMode(5,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(digitalRead(5)==0){
+    digitalWrite(4,LOW);
+  }
+   digitalWrite(4,HIGH);
+   delay(100);
+
+}
